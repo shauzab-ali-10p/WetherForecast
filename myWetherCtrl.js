df7c183d6270eef6d3eeece1a234526479d92e59
shauzi92 <a.shauzab@gmail.com>
2015-09-23 16:57:23 +0500

    fgb

--- myWetherCtrl.js
+++ myWetherCtrl.js
@@ -1,6 +1,15 @@
 app.controller("myWetherCtrl", function ($scope) {
-	$scope.fetch = function(location) {
+	var forecastCallBack = function(data){
+		console.log(data);
+		$scope.place = data.name + ", " + data.sys.country;
+		
+		var key = ['Sunrise', 'Sunset', 'Temperature', 'Pressure', 'Humidity', 'Min Temperature', 'Max Temperature']; 
+		var val = [data.sys.sunrise, data.sys.sunset, data.main.temp, data.main.pressure, data.main.humidity, data.main.temp_min, data.main.temp_max]; 
 
+		$scope.keys = key;
+		$scope.vals = val;
+	};
+	$scope.fetch = function(location) {
 		var url = "http://api.openweathermap.org/data/2.5/weather?";
 		url += "lat=" + location.lati + "&lon=" + location.longi;
 		console.log(url);
@@ -9,18 +18,6 @@ app.controller("myWetherCtrl", function ($scope) {
 			dataType: 'JSONP',
 			jsonpCallback: 'callback',
 			type: 'GET',
-			success: $scope.forecastCallBack});	
-
-	};
-	$scope.forecastCallBack = function(data){
-		console.log(data);
-		$scope.place = data.name + ", " + data.sys.country;
-		$scope.sunrise = data.sys.sunrise;
-		$scope.sunset = data.sys.sunset;
-		$scope.temp = data.main.temp;
-		$scope.pressure = data.main.pressure;
-		$scope.humidity = data.main.humidity;
-		$scope.temp_min= data.main.temp_min;
-		$scope.temp_max= data.main.temp_max;
+			success: forecastCallBack});	
 	};
 });
