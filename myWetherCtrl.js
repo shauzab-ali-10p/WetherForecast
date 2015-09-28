5619c5a4a3212517d76f9703ef3cbc0479ec4316
shauzab <shauzab.ali@tenpearls.com>
2015-09-28 18:24:01 +0500

    latest

--- myWetherCtrl.js
+++ myWetherCtrl.js
@@ -1,7 +1,7 @@
 app.controller("myWetherCtrl", function ($scope, weather) {
 	$scope.fetch = function(location) {
 
-		var result = weather.forecast(location).
+		weather.forecast(location).
 		then(function(result) {  // this is only run after $http completes
        		$scope.place = result.plac;
 			$scope.keys = result.keys;
--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ app.factory('weather', function($http) {
 	var baseUrl = "http://api.openweathermap.org/data/2.5/weather?";
 	var forecast = function(location) {
 		var myUrl = baseUrl + "lat=" + location.lati + "&lon=" + location.longi;
-		return $http({method:"GET", url:myUrl}).then(function(result){
+		return $http.get(myUrl).then(function(result){
 			var data = result.data;
         	var place = data.name + ", " + data.sys.country;
 			var key = ['Sunrise', 'Sunset', 'Temperature', 'Pressure', 'Humidity', 'Min Temperature', 'Max Temperature']; 
